b936d856c19dadb0a8aff52a08bb1cf62e6c7e34
kicok <kicoker@gmail.com>
2022-01-14 19:10:50 +0900

    검색 키워드 SharedPreferences에 저장하고 최근 검색어를 상단으로 불러오기

--- lib/src/controller/youtube_search_controller.dart
+++ lib/src/controller/youtube_search_controller.dart
@@ -1,5 +1,31 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class YoutubeSearchController extends GetxController{
-  
+class YoutubeSearchController extends GetxController {
+  String key = "searchKey";
+  RxList<String> history = RxList<String>.empty(growable: true);
+  //빈 목록을 새로 만든다. growable: true 이면 가변 리스트 (growable: true)
+
+// RxList<String> history2 = [""].obs;
+  // 위와 같은 방식으로 초기화 해도 괜찮다.
+  //RxList<String> history2 = [].obs; // [] 이렇게 처리햐면 앞에서 제네릭을 String 을 처리해준다해도 dynamic으로 인식되므로 에러
+
+  late SharedPreferences profs;
+
+  @override
+  void onInit() async {
+    profs = await SharedPreferences.getInstance();
+
+    List<String> initData = profs.getStringList(key) ?? [];
+    // 처츰 로딩 될때 SharedPreferences 에 저장된 정보를 불러와 저장한다
+    history(initData);
+
+    super.onInit();
+  }
+
+  void search(String value) {
+    history.remove(value); // 검색어 중복 제거
+    history.add(value); // 검색어를 List 마지막위치에 저장
+    profs.setStringList(key, history);
+  }
 }
--- lib/src/pages/youtube_search.dart
+++ lib/src/pages/youtube_search.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_youtube_clone_sample/src/controller/youtube_search_controller.dart';
 import 'package:get/get.dart';
 
-class YoutubeSearch extends StatelessWidget {
+class YoutubeSearch extends GetView<YoutubeSearchController> {
   const YoutubeSearch({Key? key}) : super(key: key);
 
   Widget _searchHistory() {
-    return ListView(
-      children: List.generate(
-        10,
-        (index) => ListTile(
-          leading:
-              SvgPicture.asset("assets/svg/icons/wall-clock.svg", width: 20),
-          title: Padding(
-            padding: const EdgeInsets.only(bottom: 5),
-            child: Text("개발하는 남자 $index"),
-          ),
-          trailing: const Icon(
-            Icons.arrow_forward_ios,
-            size: 15,
+    return Obx(
+      () => ListView(
+        children: List.generate(
+          controller.history.length,
+          (index) => ListTile(
+            leading:
+                SvgPicture.asset("assets/svg/icons/wall-clock.svg", width: 20),
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: Text(
+                  controller.history[controller.history.length - index - 1]),
+                  // 검색 키워드를 최근것이 상단으로 보여주기
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+              size: 15,
+            ),
           ),
         ),
       ),
@@ -27,6 +32,7 @@ class YoutubeSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Get.find<YoutubeSearchController>();
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -44,6 +50,9 @@ class YoutubeSearch extends StatelessWidget {
               borderRadius: BorderRadius.circular(5),
             ),
           ),
+          onSubmitted: (value) {
+            controller.search(value);
+          },
         ),
       ),
       body: _searchHistory(),
